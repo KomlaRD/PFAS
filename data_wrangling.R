ce15fceeac9fad3c14a9de703b951d9f0df0d1fe
KomlaRD <ankueric1@gmail.com>
2025-01-09 15:21:30 +0000

    Load usda data appropriately

--- data_wrangling.R
+++ data_wrangling.R
@@ -2,10 +2,11 @@
 pacman::p_load(
   tidyverse, # Data wrangling
   here, # File management
-  rio # Import and export dataset
+  rio, # Import and export dataset
+  DataExplorer # Create quick EDA reports
 )
 
 # Load USDA data
-usda <- import(here("data", "usda", "PFAS-Sampling-Data-2021-2022.xlsx"))
+usda <- import(here("data", "usda", "PFAS-Sampling-Data-2021-2022.xlsx"), which=2, skip = 1)
 # View first five rows
 head(usda)
