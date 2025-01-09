9f191103949174752922efd9df7671e0a9c0a699
KomlaRD <ankueric1@gmail.com>
2025-01-09 15:07:53 +0000

    Correct pacakge name for tidyverse and load usda data

--- data_wrangling.R
+++ data_wrangling.R
@@ -1,6 +1,11 @@
 # Load packages
 pacman::p_load(
-  tidverse,
-  here,
-  rio
+  tidyverse, # Data wrangling
+  here, # File management
+  rio # Import and export dataset
 )
+
+# Load USDA data
+usda <- import(here("data", "usda", "PFAS-Sampling-Data-2021-2022.xlsx"))
+# View first five rows
+head(usda)
